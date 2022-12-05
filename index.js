f583fa8b61a6bb8ad6db06a0f5f1c55f0af992d2
BlisS <fixtergeek@gmail.com>
2022-12-05 15:46:00 -0600

    initial commit 🍿

---
+++ index.js
@@ -0,0 +1,38 @@
+const data = {
+    total: 280,
+    shirts: [
+      {
+        quantity: 1,
+        id: 3,
+        title: "Invertocat Pride Tee",
+        price: 30,
+        img:
+          "https://cdn.shopify.com/s/files/1/0051/4802/products/Webshop_TShirt_Pride2022_VintageBlack_Pride_600x600_crop_center.png?v=1653680303"
+      },
+      {
+        quantity: 2,
+        id: 0,
+        title: "Youth Invertocat 4.0 Shirt",
+        price: 20,
+        img:
+          "https://cdn.shopify.com/s/files/1/0051/4802/products/WebShop_Youth_TShirt_Invertocat_4.0_Turquoise_1_600x600_crop_center.jpg?v=1629732165"
+      },
+      {
+        quantity: 3,
+        id: 1,
+        title: "Ivertocat 4.0 Shirt",
+        price: 30,
+        img:
+          "https://cdn.shopify.com/s/files/1/0051/4802/products/TShirt_Invertocat_4.0_Unisex_Black_600x600_crop_center.jpg?v=1629997801"
+      },
+      {
+        quantity: 4,
+        id: 2,
+        title: "Username 2.0 Shirt",
+        price: 30,
+        img:
+          "https://cdn.shopify.com/s/files/1/0051/4802/products/TShirt_GitHub_Username_Unisex_CoolBlue_1_600x600_crop_center.jpg?v=1629732698"
+      }
+    ]
+  };
+  
