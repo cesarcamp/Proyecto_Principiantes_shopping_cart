ba3c4f7166f6800c8c1db9d82cf24a9bf3c5f147
César Campuzano <danielsuarez8509@gmail.com>
2022-12-12 10:09:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,73 +1,95 @@
 const row = document.querySelector(".row");
 const products = document.querySelector(".products")
 const productImage = document.querySelector("img");
-const urls = {}
-
-
-// 1. recorrer los productos
-// 2. dibujar/crear un nodo row? 
-// 3. agregarle el maquetado necesario <=
-
-
 const data = {
-    total: 280,
-    shirts: [
-      {
-        quantity: 1,
-        id: 3,
-        title: "Invertocat Pride Tee",
-        price: 30,
-        img:
-          "https://cdn.shopify.com/s/files/1/0051/4802/products/Webshop_TShirt_Pride2022_VintageBlack_Pride_600x600_crop_center.png?v=1653680303"
-      },
-      {
-        quantity: 2,
-        id: 0,
-        title: "Youth Invertocat 4.0 Shirt",
-        price: 20,
-        img:
-          "https://cdn.shopify.com/s/files/1/0051/4802/products/WebShop_Youth_TShirt_Invertocat_4.0_Turquoise_1_600x600_crop_center.jpg?v=1629732165"
-      },
-      {
-        quantity: 3,
-        id: 1,
-        title: "Ivertocat 4.0 Shirt",
-        price: 30,
-        img:
-          "https://cdn.shopify.com/s/files/1/0051/4802/products/TShirt_Invertocat_4.0_Unisex_Black_600x600_crop_center.jpg?v=1629997801"
-      },
-      {
-        quantity: 4,
-        id: 2,
-        title: "Username 2.0 Shirt",
-        price: 30,
-        img:
-          "https://cdn.shopify.com/s/files/1/0051/4802/products/TShirt_GitHub_Username_Unisex_CoolBlue_1_600x600_crop_center.jpg?v=1629732698"
-      }
-    ]
-  };
+  total: 280,
+  shirts: [
+    {
+      quantity: 1,
+      id: 3,
+      title: "Invertocat Pride Tee",
+      price: 30,
+      img:
+        "https://cdn.shopify.com/s/files/1/0051/4802/products/Webshop_TShirt_Pride2022_VintageBlack_Pride_600x600_crop_center.png?v=1653680303"
+    },
+    {
+      quantity: 2,
+      id: 0,
+      title: "Youth Invertocat 4.0 Shirt",
+      price: 20,
+      img:
+        "https://cdn.shopify.com/s/files/1/0051/4802/products/WebShop_Youth_TShirt_Invertocat_4.0_Turquoise_1_600x600_crop_center.jpg?v=1629732165"
+    },
+    {
+      quantity: 3,
+      id: 1,
+      title: "Ivertocat 4.0 Shirt",
+      price: 30,
+      img:
+        "https://cdn.shopify.com/s/files/1/0051/4802/products/TShirt_Invertocat_4.0_Unisex_Black_600x600_crop_center.jpg?v=1629997801"
+    },
+    {
+      quantity: 4,
+      id: 2,
+      title: "Username 2.0 Shirt",
+      price: 30,
+      img:
+        "https://cdn.shopify.com/s/files/1/0051/4802/products/TShirt_GitHub_Username_Unisex_CoolBlue_1_600x600_crop_center.jpg?v=1629732698"
+    }
+  ]
+};
+var imagenesPlayeras=[]
+const playeras = data.shirts
 
 
 function createRow() {
   const clone = row.cloneNode(true);
   products.appendChild(clone)
-  const rows = document.querySelector(".row img")
-  //rows.src = ""
-  console.log(rows)
+
 }
+
+playeras.forEach(function(playera){
+  imagenesPlayeras.push(playera.img)
+});
+
 
 data.shirts.forEach(createRow)
-//row.innerText = null;
+console.log(productImage)
+//SOLUCION PRIMERA
+
+// const row = document.querySelector(".row");
+// const products = document.querySelector(".products")
+// const productImage = document.querySelector("img");
+// const urls = {}
 
 
-//obtiene el url de las imagenes
-for (const key in data) {
-  if (key === "shirts") {
-      for (const key2 in data[key]) {
-        urls.innerText = data[key][key2].img
-        //console.log(urls)
-      } 
-  } 
-}
+// // 1. recorrer los productos
+// // 2. dibujar/crear un nodo row? 
+// // 3. agregarle el maquetado necesario <=
+
+
+
+
+// function createRow() {
+//   const clone = row.cloneNode(true);
+//   products.appendChild(clone)
+//   const rows = document.querySelector(".row img")
+//   //rows.src = ""
+//   //console.log(rows)
+// }
+
+// data.shirts.forEach(createRow)
+// row.innerText = null;
+
+
+// //obtiene el url de las imagenes
+// for (const key in data) {
+//   if (key === "shirts") {
+//       for (const key2 in data[key]) {
+//         urls.innerText = data[key][key2].img
+//         //console.log(urls)
+//       } 
+//   } 
+// }
 
   
